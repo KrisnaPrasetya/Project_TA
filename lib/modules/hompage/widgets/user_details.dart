59f854ff6014a8813f6b9182d5f2cc05a142c3b4
KrisnaPrasetya11 <krisna2srg@gmail.com>
2025-01-14 13:12:07 +0800

    Remove unused import for flutter_svg in user_details.dart

--- lib/modules/hompage/widgets/user_details.dart
+++ lib/modules/hompage/widgets/user_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:project_ta/core/routes/app_routes.dart';
