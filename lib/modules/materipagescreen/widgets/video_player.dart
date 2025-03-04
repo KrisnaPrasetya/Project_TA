3379a53fb5ee3d7586ab7e076db146eedcd6c94d
KrisnaPrasetya11 <krisna2srg@gmail.com>
2025-03-04 19:19:17 +0800

    Enhance video player widget: add controls toggle, auto-hide functionality, and improve UI interactions

--- lib/modules/materipagescreen/widgets/video_player.dart
+++ lib/modules/materipagescreen/widgets/video_player.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:project_ta/modules/materipagescreen/widgets/fullscreenvideo.dart';
 import 'package:video_player/video_player.dart';
@@ -5,7 +7,8 @@ import 'package:video_player/video_player.dart';
 class VideoPlayerWidget extends StatefulWidget {
   final String assetPath;
 
-  const VideoPlayerWidget({Key? key, required this.assetPath}) : super(key: key);
+  const VideoPlayerWidget({Key? key, required this.assetPath})
+      : super(key: key);
 
   @override
   _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
@@ -14,6 +17,8 @@ class VideoPlayerWidget extends StatefulWidget {
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   late VideoPlayerController _controller;
   bool _isPlaying = false;
+  bool _showControls = true; // Untuk menampilkan atau menyembunyikan UI
+  late Timer _hideTimer; // Timer untuk menyembunyikan kontrol
 
   @override
   void initState() {
@@ -24,8 +29,10 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
       });
 
     _controller.addListener(() {
-      setState(() {}); // Update UI setiap perubahan video
+      setState(() {});
     });
+
+    _startHideTimer();
   }
 
   void _togglePlayPause() {
@@ -36,7 +43,31 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
         _controller.play();
       }
       _isPlaying = _controller.value.isPlaying;
+      _restartHideTimer(); // Reset timer agar UI menghilang setelah beberapa detik
     });
+  }
+
+  void _toggleControls() {
+    setState(() {
+      _showControls = !_showControls;
+    });
+
+    if (_showControls) {
+      _restartHideTimer(); // Reset timer jika user mengetuk layar
+    }
+  }
+
+  void _startHideTimer() {
+    _hideTimer = Timer(Duration(seconds: 3), () {
+      setState(() {
+        _showControls = false;
+      });
+    });
+  }
+
+  void _restartHideTimer() {
+    _hideTimer.cancel();
+    _startHideTimer();
   }
 
   void _openFullScreen() async {
@@ -48,70 +79,86 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
       ),
     );
     if (wasPlaying) {
-      _controller.play(); // Melanjutkan video jika sebelumnya sedang diputar
+      _controller.play();
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    VideoPlayer(_controller),
-                    Positioned(
-                      bottom: 10,
-                      left: 10,
-                      right: 10,
-                      child: VideoProgressIndicator(
-                        _controller,
-                        allowScrubbing: true,
-                        colors: VideoProgressColors(
-                          playedColor: Colors.red,
-                          bufferedColor: Colors.grey,
-                          backgroundColor: Colors.black,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 40,
-                      right: 10,
-                      child: IconButton(
-                        icon: Icon(Icons.fullscreen, color: Colors.white),
-                        onPressed: _openFullScreen,
-                      ),
-                    ),
-                    Positioned(
-                      child: GestureDetector(
-                        onTap: _togglePlayPause,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.black54,
-                          child: Icon(
-                            _isPlaying ? Icons.pause : Icons.play_arrow,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            : Container(
-                height: 200,
-                color: Colors.black,
-                child: Center(child: CircularProgressIndicator()),
-              ),
-      ],
-    );
   }
 
   @override
   void dispose() {
     _controller.dispose();
+    _hideTimer.cancel();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _toggleControls, // Menampilkan/Menyembunyikan UI saat diklik
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          ),
+
+          // Kontrol UI dengan animasi fade
+          AnimatedOpacity(
+            opacity: _showControls ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 500),
+            child: Container(
+              height: 217,
+              color:
+                  Colors.black.withOpacity(0.3), // Background semi transparan
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 const  SizedBox(height: 64),
+
+                  GestureDetector(
+                    onTap: _togglePlayPause,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.black54,
+                      radius: 30,
+                      child: Icon(
+                        _isPlaying ? Icons.pause : Icons.play_arrow,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                const  SizedBox(height: 44),
+
+                  // Progress bar
+                  Row(
+                    children: [
+                      // Tombol fullscreen
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: VideoProgressIndicator(
+                          _controller,
+                          allowScrubbing: true,
+                          colors: VideoProgressColors(
+                            playedColor: Colors.red,
+                            bufferedColor: Colors.grey,
+                            backgroundColor: Colors.black,
+                          ),
+                        ),
+                      ),
+
+                      // Tombol fullscreen
+                      IconButton(
+                        icon: Icon(Icons.fullscreen, color: Colors.white),
+                        onPressed: _openFullScreen,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
